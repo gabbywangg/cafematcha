efb3037af93ed365ca8ca054a6905af4142559ad
gabbywangg <gabriellewang@ucsb.edu>
2025-09-10 14:47:02 -0700

    Update drinking.js

--- drinking.js
+++ drinking.js
@@ -44,11 +44,13 @@ cnv.parent("sketch");
 function draw() {
   background(bg);
 
+  // draw the matcha cup depending on state
   if (cupState === "fullmatcha") image(fullmatcha, matchaX, matchaY, matchaW, matchaH);
   if (cupState === "halfmatcha") image(halfmatcha, matchaX, matchaY, matchaW, matchaH);
   if (cupState === "onefourthmatcha") image(onefourthmatcha, matchaX, matchaY, matchaW, matchaH);
   if (cupState === "fullice") image(fullice, matchaX, matchaY, matchaW, matchaH);
 
+  // show mail if dialog 1 is active
   if (dialogState === 1 && !showLetter) {
     showMail = true;
   }
@@ -61,11 +63,13 @@ function draw() {
     image(letter, 225, 50, 350, 400);
   }
 
+  // draw dialog box if active
   if (showDialog) {
     drawDialog(dialogs[dialogState]);
   }
 }
 
+// Toggle button hookup
 window.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("toggle-sound");
   toggleBtn.addEventListener("click", () => {
@@ -76,6 +80,11 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
 function drawDialog(txt) {
+  let boxX = 50;
+  let boxY = height - 120;
+  let boxW = width - 100;
+  let boxH = 80;
+  
  if (mouseX > mailX && mouseX < mailX + mailW &&
         mouseY > mailY && mouseY < mailY + mailH) {
       push();
@@ -85,19 +94,23 @@ function drawDialog(txt) {
       rect(mailX - 1, mailY - 1, mailW + 2, mailH + 2, 2);
       pop();
  }
+  // green rectangle background
    fill(110, 142, 83, 230);
       noStroke();
-  rect(50, 380, 700, 80, 20);
+  rect(boxX, boxY, boxW, boxH, 20);
 
+  // dialog text
   fill(255);
   text(txt, boxX + boxW / 2, boxY + boxH / 2);
 }
 
 function mousePressed() {
+  // play plop every click
   if (plop && soundEnabled && !plop.isPlaying()) {
   plop.play();
 }
 
+  // drink matcha
   if (
     dialogState == 0 &&
     mouseX > matchaX &&
@@ -115,6 +128,8 @@ function mousePressed() {
       showDialog = true;
     }
   }
+
+  // click mail to open letter
   if (
     showMail &&
     mouseX > mailX &&
@@ -124,7 +139,7 @@ function mousePressed() {
   ) {
     showMail = false;
     showLetter = true;
-    showDialog = false;
+    showDialog = false; // hide dialog when letter opens
 
     if (celly && soundEnabled) {
   celly.play();
